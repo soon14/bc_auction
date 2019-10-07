635e029c9fe6de1c2117a0045a54c07bd703e1bf
Haeje Park <hj123190@gmail.com>
2019-10-07 19:38:33 +0900

    경매 목록페이지 경매 종료, 시작 정표 표시

--- haribo-frontend/pages/auction.vue.js
+++ haribo-frontend/pages/auction.vue.js
@@ -15,7 +15,7 @@ var auctionView = Vue.component('AuctionView', {
                             <div class="card-body">
                                 <img src="./assets/images/artworks/artwork1.jpg">
                                 <h4>{{ item['auction_goodsid']['art_name'] }}</h4>
-                                <p>{{ calculateDate(item['auction_end']) }}</p>
+                                <p>{{ calculateDate(item['auction_start'], item['auction_end'] ) }}</p>
                                 <router-link :to="{ name: 'auction.detail', params: { id: item['auction_id'] }}" class="btn btn-block btn-secondary">자세히보기</router-link>
                             </div>
                         </div>
@@ -30,31 +30,41 @@ var auctionView = Vue.component('AuctionView', {
         }
     },
     methods: {
-        calculateDate(date) {
-            var now = new Date().getTime();
-            var endDate = new Date(date);
-
-            endDate=endDate.setHours(endDate.getHours()+9)
-
-            var diff = endDate-now;
-            
-
-            // 만약 종료일자가 지났다면 "경매 마감"을 표시한다.
-            if(diff < 0) {
-                return "경매 마감";
-            } else {
-                // UNIX Timestamp를 자바스크립트 Date객체로 변환한다.
-                var difference = diff / 1000;
-                // var secs = difference % 60
+        calcTime(time){
+            var difference = time / 1000;
+                var secs = difference % 60
                 difference = parseInt(difference / 60)
                 var minutes = difference % 60
                 difference = parseInt(difference / 60)
                 var hours = difference % 24
                 difference = parseInt(difference / 24)
-                var days = difference
+                var days = difference;
 
-                return "남은시간: " + days + "일 " + hours + "시간 " + minutes + "분";
-                // return timeSince(-1*endDate);
+                var timeInfo = {'day' : days, 'hour' : hours, 'minute' : minutes, 'sec': secs};
+                return timeInfo;
+        },
+        calculateDate(sdate, edate) {
+            var now = new Date().getTime();
+            var startDate = new Date(sdate);
+            var endDate = new Date(edate);
+
+            endDate=endDate.setHours(endDate.getHours()+9);
+            var plus9StartDate = startDate.setHours(startDate.getHours() + 9);
+           
+            var endDiff = endDate-now;
+
+            // 만약 종료일자가 지났다면 "경매 마감"을 표시한다.
+            if( startDate > now) {
+                var startDiff = startDate - now;
+                var timeInfo = this.calcTime(startDiff);
+                return "경매 예정 ( " + timeInfo.hour + "h" + timeInfo.minute + "m 후 )";
+            }
+            if(endDiff < 0) {
+                return "경매 마감";
+            } else {
+                // UNIX Timestamp를 자바스크립트 Date객체로 변환한다.
+                var timeInfo = this.calcTime(endDiff);
+                return "남은시간: " + timeInfo.day + "일 " + timeInfo.hour + "시간 ";
             }
         }
     },
